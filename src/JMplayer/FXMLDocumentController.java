6051b916b0074d611038207f53dc3fad62e41e15
Mthobeli Makaluza <mthobelimakaluza@gmail.com>
2021-04-22 17:01:24 +0200

    open file coded

--- src/JMplayer/FXMLDocumentController.java
+++ src/JMplayer/FXMLDocumentController.java
@@ -1,18 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmediaplayer;
 
-package JMplayer;
-
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
 
-/**
- * FXML Controller class
- */
+
 public class FXMLDocumentController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    
+    private MediaPlayer mediaplayer;
+    
+    private String filepath;
+    
+    @FXML
+    private MediaView mediaView;
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    //This method enables us to create a open file button for the media player
+    //The Open file button allows us to access the files on your computer so that you can open the video you want
+    private void handleButtonAction(ActionEvent event) {
+        FileChooser filechooser = new FileChooser();
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Please select a file (*.mp4)","*.mp4");
+            filechooser.getExtensionFilters().add(filter);
+            File file = filechooser.showOpenDialog(null);
+            filepath = file.toURI().toString();
+            
+            if(filepath != null){
+                
+                Media media = new Media(filepath);
+                mediaplayer = new MediaPlayer(media);
+                mediaView.setMediaPlayer(mediaplayer);
+                    DoubleProperty width = mediaView.fitWidthProperty();
+                    DoubleProperty height = mediaView.fitHeightProperty();
+                    
+                    width.bind(Bindings.selectDouble(mediaView.sceneProperty(), "width"));
+                    height.bind(Bindings.selectDouble(mediaView.sceneProperty(), "height"));
+                    
+                    
+                
+                
+                mediaplayer.play();
+            }
+            
+            
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
--- src/JMplayer/JMplayer.java
+++ src/JMplayer/JMplayer.java
@@ -1,4 +1,5 @@
-package JMplayer;
+
+package jmediaplayer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,17 +7,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class JMplayer extends Application {
-    
-    public static void main(String[] args) {
-        launch(args);
-    }
 
+public class JMediaPlayer extends Application {
+    
     @Override
+    //In this method we start running the GUI of the media player
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
         stage.show();
     }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
 }
